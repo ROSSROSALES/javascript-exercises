d7d17d543221fe2efea33cfbcc70ed3eeada202a
Ross Rosales <52366381+ROSSROSALES@users.noreply.github.com>
2024-05-10 22:05:46 +0000

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+/*
+const removeFromArray = funciton(array, ...args) {
+    
+}
+*/
 
+const removeFromArray = function() {
+    let result = [];
+    let array = arguments[0];
+    let hashSet = new Set();
+
+    // add ...args to hashmap
+    for (j=1; j < arguments.length; j++) {
+        hashSet.add(arguments[j])
+    }
+    for (i=0; i < array.length; i++) {
+        if (!hashSet.has(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
