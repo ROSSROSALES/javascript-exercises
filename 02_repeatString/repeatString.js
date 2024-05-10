52c37bec2e39679d5dd78c7819e4dec49d7e7e33
Ross Rosales <52366381+ROSSROSALES@users.noreply.github.com>
2024-05-10 01:20:29 +0000

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (i = 0; i < numberOfRepeats; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
