a0bcecc0f928306a60f658c9a5dbf6e0d4d642f6
Ross Rosales <ross.erosales@gmail.com>
2024-10-28 19:22:06 -0400

    complete exercise08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0
+  }
+  return array.reduce((total,n) => {
+    return total+n
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,n) => {
+    return total*n
+  });
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n==0 || n==1) {
+    return 1
+  }
+  var total = 1
+  while (n>1) {
+    total *= n
+    n-=1
+  }
+  return total
 };
 
 // Do not edit below this line
